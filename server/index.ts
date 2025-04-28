28453ba7e9dffec9301f18cb8d4935adc70306e2
Sam Sena <samsena@gmail.com>
2025-04-28 04:43:11 -0500

    chore: simplify server/index.ts for basic execution test

--- server/index.ts
+++ server/index.ts
@@ -1,5 +1,6 @@
-console.log('--- [DEBUG] server/index.ts starting execution ---');
+console.log('--- [DEBUG] server/index.ts minimal execution test ---');
 
+/*
 import express, { type Request, Response, NextFunction } from "express";
 import { registerRoutes } from "./routes";
 import { setupVite, serveStatic, log } from "./vite";
@@ -116,3 +117,4 @@ process.on('uncaughtException', (err: NodeJS.ErrnoException) => {
   }
   process.exit(1);
 });
+*/
